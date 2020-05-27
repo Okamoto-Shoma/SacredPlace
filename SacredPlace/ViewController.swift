937cfe4c70d787cde7c33616a70d1aa2c9531b40
s-okamoto <s-okamoto@i-enter.co.jp>
2020-05-27 16:07:10 +0900

    Initial Commit

---
+++ SacredPlace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SacredPlace
+//
+//  Created by 岡本 翔真 on 2020/05/27.
+//  Copyright © 2020 shoma.okamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
